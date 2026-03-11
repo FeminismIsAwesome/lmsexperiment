24fa809a7106218ba6b28e4d3745694beca1e8db
Ian Norris <iannorris@MacBook-Air-4.local>
2026-03-11 16:39:05 -0700

    experimetn

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,6 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
+bundle exec rails SECRET_KEY_BASE_DUMMY=1  assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
